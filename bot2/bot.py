695f159bfb3ccd5ac09a93236d2d6a7da277b0a2
ListaSara <136308823+ListaSara@users.noreply.github.com>
2023-06-22 23:15:34 +0200

    Add files via upload

--- bot2/bot.py
+++ bot2/bot.py
@@ -17,101 +17,295 @@ def execute_query(query):
 def format_results(results):
     formatted = ''
     for row in results:
-        formatted += "ID: {}, Username: {}, Name: {}\n".format(row[0], row[1], row[2])
+        formatted += "NOME STRUTTURA: {}\n, INDIRIZZO: {}\n\n".format(row[0], row[1])
     return formatted
 
 from telegram.ext import Updater, CommandHandler
 
-#bottone 1
+#bottone 1 - Bussolengo
 async def bott1(update, context):
-    query = "SELECT * FROM Bussolengo"
+    # Creazione dei bottoni
+    kbBus = [
+        [InlineKeyboardButton("Agriturismo", callback_data='bus_agri')],
+        [InlineKeyboardButton("B&B", callback_data='bus_bb')],
+        [InlineKeyboardButton("Hotel", callback_data='bus_hotel')]
+    ]
+    reply_markup = InlineKeyboardMarkup(kbBus)
+    # Invio dei bottoni all'utente
+    await update.callback_query.message.edit_text(text='Bussolengo:', reply_markup=reply_markup)
+
+async def bus_bb(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Bussolengo where tipostruttura in ('Locazione turistica','Casa vacanza','Bed and Breakfast','Affittacamere')"
     results = execute_query(query)
     formatted_results = format_results(results)
-    response = "Elenco utenti:\n" + formatted_results
+    response = "Elenco B&B:\n" + formatted_results
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
-#bottone 2
+async def bus_hotel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Bussolengo where tipostruttura in ('Albergo, Villaggio albergo, Albergo diffuso Residenze turistiche alberghiere Campeggi, villaggi turistici')"
+    results = execute_query(query)
+    formatted_results = format_results(results)
+    response = "Elenco Hotel:\n" + formatted_results
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+async def bus_agri(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Bussolengo where tipostruttura in ('Agriturismo')"
+    results = execute_query(query)
+    formatted_results = format_results(results)
+    response = "Elenco Agriturismo:\n" + formatted_results
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+#bottone 2 - Castelnuovo
 async def bott2(update, context):
-    query = "SELECT * FROM Castelnuovo"
+    # Creazione dei bottoni
+    kbCas = [
+        [InlineKeyboardButton("Agriturismo", callback_data='cas_agri')],
+        [InlineKeyboardButton("B&B", callback_data='cas_bb')],
+        [InlineKeyboardButton("Hotel", callback_data='cas_hotel')]
+    ]
+    reply_markup = InlineKeyboardMarkup(kbCas)
+    # Invio dei bottoni all'utente
+    await update.callback_query.message.edit_text(text='Castelnuovo:', reply_markup=reply_markup)
+
+async def cas_bb(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Castelnuovo where tipostruttura in ('Casa vacanza','Bed and Breakfast', 'Locanda','Campeggio','Affittacamere') limit 5"
     results = execute_query(query)
     formatted_results = format_results(results)
-    response = "Elenco utenti:\n" + formatted_results
+    response = "Elenco B&B:\n" + formatted_results
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
-#bottone 3
-async def bott3(update, context):
-    keyboard = [
-        [InlineKeyboardButton("Bussolengo", callback_data='button1')],
-        [InlineKeyboardButton("Castelnuovo", callback_data='button2')],
-        [InlineKeyboardButton("Mozzecane", callback_data='button3')]]
-    if query.data == 'button1':
-        print("1")
-    elif query.data == 'button2':
-        print("2")
-    elif query.data == 'button3':
-        print("3")
-        '''
-    query = "SELECT * FROM Mozzecane"
+async def cas_hotel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Castelnuovo where tipostruttura in ('Albergo', 'Altre strutture extralberghiere', 'Villaggio','Residence')"
     results = execute_query(query)
     formatted_results = format_results(results)
-    response = "Elenco utenti:\n" + formatted_results
+    response = "Elenco Hotel:\n" + formatted_results
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
-    '''
 
-#bottone 4
+async def cas_agri(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Castelnuovo where tipostruttura in ('Agriturismo')"
+    results = execute_query(query)
+    formatted_results = format_results(results)
+    response = "Elenco Agriturismo:\n" + formatted_results
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+#bottone 3 _ Mozzecane
+async def bott3(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Creazione dei bottoni
+    kbMozz = [
+        [InlineKeyboardButton("BB", callback_data='moz_bb')],
+        [InlineKeyboardButton("Hotel", callback_data='moz_hotel')]
+    ]
+    reply_markup = InlineKeyboardMarkup(kbMozz)
+    # Invio dei bottoni all'utente
+    await update.callback_query.message.edit_text(text='Mozzecane:', reply_markup=reply_markup)
+
+async def moz_bb(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Mozzecane where tipostruttura = 'B&B'"
+    results = execute_query(query)
+    formatted_results = format_results(results)
+    response = "Elenco B&B:\n" + formatted_results
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+async def moz_hotel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Mozzecane where tipostruttura = 'Alberghiero'"
+    results = execute_query(query)
+    formatted_results = format_results(results)
+    response = "Elenco Alberghi:\n" + formatted_results
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+#bottone 4 - Pastrengo
 async def bott4(update, context):
-    query = "SELECT * FROM Pastrengo"
+    # Creazione dei bottoni
+    kbPas = [
+        [InlineKeyboardButton("B&B", callback_data='pas_bb')],
+        [InlineKeyboardButton("Hotel", callback_data='pas_hotel')]
+    ]
+    reply_markup = InlineKeyboardMarkup(kbPas)
+    # Invio dei bottoni all'utente
+    await update.callback_query.message.edit_text(text='Pastrengo:', reply_markup=reply_markup)
+
+async def pas_bb(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Pastrengo where tipostruttura in ('B&B')"
     results = execute_query(query)
     formatted_results = format_results(results)
-    response = "Elenco utenti:\n" + formatted_results
+    response = "Elenco B&B:\n" + formatted_results
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
-#bottone 5
+async def pas_hotel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Pastrengo where tipostruttura in ('Alberghiero')"
+    results = execute_query(query)
+    formatted_results = format_results(results)
+    response = "Elenco Hotel:\n" + formatted_results
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+
+#bottone 5 - Pescantina
 async def bott5(update, context):
-    query = "SELECT * FROM Pescantina"
+# Creazione dei bottoni
+    kbPes = [
+        [InlineKeyboardButton("B&B", callback_data='pes_bb')],
+        [InlineKeyboardButton("Hotel", callback_data='pes_hotel')]
+    ]
+    reply_markup = InlineKeyboardMarkup(kbPes)
+    # Invio dei bottoni all'utente
+    await update.callback_query.message.edit_text(text='Pescantina:', reply_markup=reply_markup)
+
+async def pes_bb(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Pescantina where tipostruttura in ('B&B')"
     results = execute_query(query)
     formatted_results = format_results(results)
-    response = "Elenco utenti:\n" + formatted_results
+    response = "Elenco B&B:\n" + formatted_results
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
-#bottone 6
+async def pes_hotel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Pescantina where tipostruttura in ('Hotel')"
+    results = execute_query(query)
+    formatted_results = format_results(results)
+    response = "Elenco Hotel:\n" + formatted_results
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+#bottone 6 - Sommacampagna
 async def bott6(update, context):
-    query = "SELECT * FROM Sommacampagna"
+    # Creazione dei bottoni
+    kbSom = [
+        [InlineKeyboardButton("Agriturismo", callback_data='som_agri')],
+        [InlineKeyboardButton("B&B", callback_data='som_bb')],
+        [InlineKeyboardButton("Hotel", callback_data='som_hotel')]
+    ]
+    reply_markup = InlineKeyboardMarkup(kbSom)
+    # Invio dei bottoni all'utente
+    await update.callback_query.message.edit_text(text='Sommacampagna:', reply_markup=reply_markup)
+
+async def som_bb(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Sommacampagna where tipostruttura in ('B&B','LOCAZIONE TURISTICA', 'ALLOGGIO TURISTICO','STRUTTURA TURISTICA CLASSIFICATA')"
     results = execute_query(query)
     formatted_results = format_results(results)
-    response = "Elenco utenti:\n" + formatted_results
+    response = "Elenco B&B:\n" + formatted_results
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
-#bottone 7
+async def som_hotel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Sommacampagna where tipostruttura in ('STRUTTURA RICETTIVA ALBERGHIERA', 'STRUTTURA RICETTIVA COMPLEMENTARE', 'STRUTTURA RICETTIVA COMPLEMENTARE CLASSIFICATA')"
+    results = execute_query(query)
+    formatted_results = format_results(results)
+    response = "Elenco Hotel:\n" + formatted_results
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+async def som_agri(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Sommacampagna where tipostruttura in ('AGRITURISMO')"
+    results = execute_query(query)
+    formatted_results = format_results(results)
+    response = "Elenco Agriturismo:\n" + formatted_results
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+
+#bottone 7 - Sona
 async def bott7(update, context):
-    query = "SELECT * FROM Sona"
+    # Creazione dei bottoni
+    kbSo = [
+        [InlineKeyboardButton("Agriturismo", callback_data='so_agri')],
+        [InlineKeyboardButton("B&B", callback_data='so_bb')]
+    ]
+    reply_markup = InlineKeyboardMarkup(kbSo)
+    # Invio dei bottoni all'utente
+    await update.callback_query.message.edit_text(text='Sona:', reply_markup=reply_markup)
+
+async def so_bb(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Sona where tipostruttura in ('B&B')"
     results = execute_query(query)
     formatted_results = format_results(results)
-    response = "Elenco utenti:\n" + formatted_results
+    response = "Elenco B&B:\n" + formatted_results
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
-#bottone 8
+
+async def so_agri(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Sona where tipostruttura in ('Alberghiero')"
+    results = execute_query(query)
+    formatted_results = format_results(results)
+    response = "Elenco Agriturismo:\n" + formatted_results
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+#bottone 8 - Valeggio
 async def bott8(update, context):
-    query = "SELECT * FROM Valeggio"
+    # Creazione dei bottoni
+    kbVal = [
+        [InlineKeyboardButton("Agriturismo", callback_data='val_agri')],
+        [InlineKeyboardButton("B&B", callback_data='val_bb')]
+    ]
+    reply_markup = InlineKeyboardMarkup(kbVal)
+    # Invio dei bottoni all'utente
+    await update.callback_query.message.edit_text(text='Valeggio:', reply_markup=reply_markup)
+
+async def val_bb(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Valeggio where tipostruttura in ('B&B')"
     results = execute_query(query)
     formatted_results = format_results(results)
-    response = "Elenco utenti:\n" + formatted_results
+    response = "Elenco B&B:\n" + formatted_results
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
-#bottone 9
+async def val_agri(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Valeggio where tipostruttura in ('Alberghiero')"
+    results = execute_query(query)
+    formatted_results = format_results(results)
+    response = "Elenco Agriturismo:\n" + formatted_results
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+#bottone 9 - Villafranca
 async def bott9(update, context):
-    query = "SELECT * FROM Villafranca"
+    # Creazione dei bottoni
+    kbVil = [
+        [InlineKeyboardButton("Agriturismo", callback_data='vil_agri')],
+        [InlineKeyboardButton("B&B", callback_data='vil_bb')],
+        [InlineKeyboardButton("Hotel", callback_data='vil_hotel')]
+    ]
+    reply_markup = InlineKeyboardMarkup(kbVil)
+    # Invio dei bottoni all'utente
+    await update.callback_query.message.edit_text(text='Villafranca:', reply_markup=reply_markup)
+
+async def vil_bb(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Villafranca where tipostruttura in ('B&B','Affittacamere', 'Locazioni turistiche','Unità abitative')"
     results = execute_query(query)
     formatted_results = format_results(results)
-    response = "Elenco utenti:\n" + formatted_results
+    response = "Elenco B&B:\n" + formatted_results
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
-#bottone 10
-async def bott10(update, context):
-    query = "SELECT * FROM Vigasio"
+async def vil_hotel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Villafranca where tipostruttura in ('Alberghiero', '', '')"
     results = execute_query(query)
     formatted_results = format_results(results)
-    response = "Elenco utenti:\n" + formatted_results
+    response = "Elenco Hotel:\n" + formatted_results
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+async def vil_agri(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Villafranca where tipostruttura in ('Agriturismo','Enoturismo', 'Fattorie Sociali','Agricampeggio','Turismo rurale')"
+    results = execute_query(query)
+    formatted_results = format_results(results)
+    response = "Elenco Agriturismo:\n" + formatted_results
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+#bottone 10 - Vigasio
+async def bott10(update, context):
+    # Creazione dei bottoni
+    kbVi = [
+        [InlineKeyboardButton("Agriturismo", callback_data='vi_agri')],
+        [InlineKeyboardButton("B&B", callback_data='vi_bb')]
+    ]
+    reply_markup = InlineKeyboardMarkup(kbVi)
+    # Invio dei bottoni all'utente
+    await update.callback_query.message.edit_text(text='Vieggio:', reply_markup=reply_markup)
+
+async def vi_bb(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Vigasio where tipostruttura in ('B&B')"
+    results = execute_query(query)
+    formatted_results = format_results(results)
+    response = "Elenco B&B:\n" + formatted_results
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+
+async def vi_agri(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = "SELECT * FROM Vigasio where tipostruttura in ('Alberghiero')"
+    results = execute_query(query)
+    formatted_results = format_results(results)
+    response = "Elenco Agriturismo:\n" + formatted_results
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
 
@@ -156,13 +350,17 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         [InlineKeyboardButton("Sona", callback_data='button7')],
         [InlineKeyboardButton("Valeggio", callback_data='button8')],
         [InlineKeyboardButton("Villafranca", callback_data='button9')],
-        [InlineKeyboardButton("Vigasio", callback_data='button1')],
+        [InlineKeyboardButton("Vigasio", callback_data='button10')],
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
     
     # Invio dei bottoni all'utente
     await update.message.reply_text('Clicca su uno dei bottoni:', reply_markup=reply_markup)
+
+
+
+
 
 # Funzione per gestire la pressione dei bottoni
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -191,7 +389,66 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif query.data == 'button10':
         await bott10(update, context)
 
+    elif query.data == 'moz_bb':
+        await moz_bb(update, context)
+    elif query.data == 'moz_hotel':
+        await moz_hotel(update, context)
+
+    elif query.data == 'som_agri':
+        await som_agri(update, context)
+    elif query.data == 'som_bb':
+        await som_bb(update, context)
+    elif query.data == 'som_hotel':
+        await som_hotel(update, context)
+
+    elif query.data == 'so_agri':
+        await so_agri(update, context)
+    elif query.data == 'so_bb':
+        await so_bb(update, context)
+
+    elif query.data == 'val_agri':
+        await val_agri(update, context)
+    elif query.data == 'val_bb':
+        await val_bb(update, context)
+
+    elif query.data == 'vi_agri':
+        await vi_agri(update, context)
+    elif query.data == 'vi_bb':
+        await vi_bb(update, context)
     
+    elif query.data == 'cas_agri':
+        await cas_agri(update, context)
+    elif query.data == 'cas_bb':
+        await cas_bb(update, context)
+    elif query.data == 'cas_hotel':
+        await cas_hotel(update, context)
+
+    elif query.data == 'bus_agri':
+        await bus_agri(update, context)
+    elif query.data == 'bus_bb':
+        await bus_bb(update, context)
+    elif query.data == 'bus_hotel':
+        await bus_hotel(update, context)
+    
+    elif query.data == 'pas_bb':
+        await pas_bb(update, context)
+    elif query.data == 'pas_hotel':
+        await pas_hotel(update, context)
+
+    elif query.data == 'pes_bb':
+        await pes_bb(update, context)
+    elif query.data == 'pes_hotel':
+        await pes_hotel(update, context)
+
+    elif query.data == 'vil_agri':
+        await vil_agri(update, context)
+    elif query.data == 'vil_bb':
+        await vil_bb(update, context)
+    elif query.data == 'vil_hotel':
+        await vil_hotel(update, context)
+    
+
+
 # IL MAIN CONTIENE TUTTI I COMANDI CHE L'UTENTE PUò ESEGUIRE
 def main():
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
